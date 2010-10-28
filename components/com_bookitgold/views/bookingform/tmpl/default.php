f4748e31fca19130b8690c01477dd63b06160e91
Renji NsK <renji@thessite.gr>
2010-10-28 18:10:37 +0300

    HTML Error fix

--- components/com_bookitgold/views/bookingform/tmpl/default.php
+++ components/com_bookitgold/views/bookingform/tmpl/default.php
@@ -607,9 +607,6 @@ for ($i=0; $i<count($pref_array); $i++)
 <div class="bookit_booking_clear"></div>
 
 <div class="bookit_booking_review_header"><?php echo JText::_("Extra Services");//echo JText::_("Taxes / Extra Services");?>
-<?php
-echo JText::_("In an attempt to make your stay as comfortable as possible, a list of extra services is given below for you to choose the one(s) that meet your needs and expectations");
-?>
 
 <br />
 
@@ -702,7 +699,9 @@ for ($i=0; $i<count($extra_array); $i++)
 }
 
 ?></div>
-
+<?php
+echo JText::_("In an attempt to make your stay as comfortable as possible, a list of extra services is given below for you to choose the one(s) that meet your needs and expectations");
+?>
 
 
 <div class="bookit_booking_clear"></div>
